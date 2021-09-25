7c48671e4279a4fc9193ac385ad775f404401f0a
Fanny Rodolakis <rodolakis@anl.gov>
2021-09-25 00:55:08 -0500

    add IEX_29id/utils/plot.py

---
+++ IEX_29id/utils/plot.py
@@ -0,0 +1,17 @@
+from epics import caput, caget 
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+
+
+
+#  filepath='/home/beams/29IDUSER/Documents/User_Folders/Topp/S089.tif'
+def plot_image(filepath,h=20,v=10):
+    """
+    filepath = '/home/beams/29IDUSER/Documents/User_Folders/UserName/TifFile.tif'
+    """
+    image = mpimg.imread(filepath)
+    plt.figure(figsize=(h,v))
+    #plt.imshow(image,cmap='gray',vmin=v1,vmax=v2)
+    plt.imshow(image,cmap='gray')
+    plt.axis('off')
+    plt.show()
