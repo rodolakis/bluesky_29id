67e0f2b1931aa0a57a2b42e92b022bf9924702e2
Fanny Rodolakis <rodolakis@anl.gov>
2021-09-27 17:45:35 -0500

    create devices.detector.py

---
+++ IEX_29id/devices/detector.py
@@ -0,0 +1,46 @@
+from epics import caput,caget
+from time import sleep
+from ..utils.strings import ClearCalcOut
+
+           
+def Kappa_DetectorDict(which):
+    """
+    which = d3, d4, mcp, apd, yag
+    """
+    #det={'d4': 0.0,'d3': 7.684, 'mcp': 18.275, 'apd': 29.753, 'yag': 33.269, }   # up to 2020_3
+    det={'d4': 0.0,'d3': -29.123, 'mcp': -18.6416-0.2+0.11, 'apd': 0.0, 'yag': 4.036 }    # 2021_1
+    angle=det[which]
+    return angle
+
+
+def setdet(which):
+    """
+    which = d3, d4, mcp, apd, yag
+    Reset tth for a given detector.
+    """
+    detset(which)
+
+def detset(which):
+    """
+    which = d3, d4, mcp, apd, yag
+    Reset tth for a given detector.
+    """
+    caput('29idKappa:det:set',which)
+
+    
+def MPA_Interlock():
+    ioc="Kappa"
+    n=7
+    pvioc="29id"+ioc+":userCalcOut"+str(n)
+    ClearCalcOut(ioc,n)
+    LLM=-16
+    HLM=-6
+    caput(pvioc+".DESC","MPA Interlock")
+    caput(pvioc+".INPA","29idKappa:m9.DRBV CP NMS")
+    caput(pvioc+".B",1)
+    caput(pvioc+".CALC$","ABS((("+str(LLM)+"<A && A<"+str(HLM)+") && (B>0))-1)")
+    caput(pvioc+".OCAL$",'A')
+    caput(pvioc+".OOPT",2)    # When zero
+    caput(pvioc+".DOPT",0)    # Use CALC
+    caput(pvioc+".IVOA",0)    # Continue Normally
+    caput(pvioc+".OUT","29iddMPA:C0O PP NMS")
