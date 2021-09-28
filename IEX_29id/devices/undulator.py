548b23e04d3ac5bb23becd60d3dcfa147c963f57
Fanny Rodolakis <rodolakis@anl.gov>
2021-09-28 12:02:26 -0500

    start organizing undulator functions

--- IEX_29id/devices/undulator.py
+++ IEX_29id/devices/undulator.py
@@ -1,2 +1,18 @@
 from epics import caget, caput
 from time import sleep
+
+
+
+def ID_State2Mode(which,mode):
+    ID_State2Mode={}
+    ID_State2Mode["Mode"]  = {"RCP":0, "LCP":1, "V":2, "H":3, "HN":4}
+    ID_State2Mode["State"] = {0:"RCP", 1:"LCP", 2:"V", 3:"H", 4:"HN"}
+    try:
+        ID=ID_State2Mode[which][mode]
+    except KeyError:
+        print("WARNING: Not a valid ID mode!")
+    return ID
+
+#replace with    ?
+#   ID_Mode_list=['RCP','LCP','V','H','HN']
+#   ID_Mode=ID_Mode_list[caget("ID29:ActualMode")]
