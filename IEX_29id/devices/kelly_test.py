6bfdac933c46d667962882a53a88c39539e83e3a
kelly <kellyjelly0904@gmail.com>
2021-11-10 19:00:32 -0800

    kelly test slit

--- IEX_29id/devices/kelly_test.py
+++ IEX_29id/devices/kelly_test.py
@@ -6,74 +6,76 @@ from ophyd import Component, Device
 #from hkl.geometries import E4CV
 
 #logging.basicConfig(level=logging.WARNING)
+
 logger = logging.getLogger(__name__)
 
-class MyEpicsMotor(EpicsMotor):
-    sync = Component(EpicsSignal, "sync.PROC")
-    desc = Component(EpicsSignalRO,".DESC")
-    homf = Component(EpicsSignal,".HOMF")
-    dval = Component(EpicsSignal,".DVAL")
 
 class _SlitsMotors(Device):
-    m1  = Component(MyEpicsMotor, "1")    ## 1H  
-    m2  = Component(MyEpicsMotor, "2")    ## 1V
-    m3  = Component(MyEpicsMotor, "3")    ## 2H
-    m4  = Component(MyEpicsMotor, "4")    ## 2V
+    m9   = Component(EpicsMotor, "9")    ## top1
+    m10  = Component(EpicsMotor, "10")    ## in1
+    m11  = Component(EpicsMotor, "11")    ## out1
+    m12  = Component(EpicsMotor, "12")    ## bot1
+    m13  = Component(EpicsMotor, "13")    ## top2
+    m14  = Component(EpicsMotor, "14")    ## in2
+    m15  = Component(EpicsMotor, "15")    ## out2
+    m16  = Component(EpicsMotor, "16")    ## bot2
 
 slits_motors = _SlitsMotors("29idb:m", name="motors")
 
-class _SoftMotor(PVPositionerPC):
-    setpoint = Component(EpicsSignal, "")
-    readback = Component(EpicsSignalRO, ".RBV")   
-    desc = Component(EpicsSignalRO,".DESC")
+class _SoftSize(PVPositionerPC):
+    setpoint = Component(EpicsSignal, "size.VAL")  
+    readback = Component(EpicsSignalRO, "t2.C")   
+    sync = Component(EpicsSignal, "sync.PROC")
+class _SoftCenter(PVPositionerPC):
+    setpoint = Component(EpicsSignal, "center.VAL")  
+    readback = Component(EpicsSignalRO, "t2.D")   
+    sync = Component(EpicsSignal, "sync.PROC")
 
 class _FourMotors(Device):
-    Slit1H = Component(_SoftMotor, "29idb:Slit1H")   
-    Slit1V = Component(_SoftMotor, "29idb:Slit1V")    
-    Slit2H = Component(_SoftMotor, "29idb:Slit2H")      
-    Slit2V = Component(_SoftMotor, "29idb:Slit2V")
+    size_1H = Component(_SoftSize, "1H")   
+    size_1V = Component(_SoftSize, "1V")  
+    size_2H = Component(_SoftSize, "2H")   
+    size_2V = Component(_SoftSize, "2V")  
+    center_2H = Component(_SoftCenter, "2H")      
+    center_2V = Component(_SoftCenter, "2V")
+    center_1H = Component(_SoftCenter, "1H")      
+    center_1V = Component(_SoftCenter, "1V")
 
-four_motors = _FourMotors("",name="motors")
- 
-def sync_PI_motors():
-    """
-    Synchronize VAL with RBV for 1H, 1V, 2H, 2V
-    """
-    Slit1H_motor = slits_motors.m1
-    Slit1V_motor = slits_motors.m2
-    Slit2H_motor = slits_motors.m3
-    Slit2V_motor = slits_motors.m4
-    yield from bps.abs_set(Slit1H_motor.sync,1)
-    yield from bps.abs_set(Slit1V_motor.sync,1)
-    yield from bps.abs_set(Slit2H_motor.sync,1)
-    yield from bps.abs_set(Slit2V_motor.sync,1)
+slits = _FourMotors("29idb:slit",name="motors")
 
-class _Status(Device):
-    st1  = Component(EpicsSignal, "1")        
-    st2  = Component(EpicsSignal, "2")    
-    st3  = Component(EpicsSignal, "3")     
-    st4  = Component(EpicsSignal, "4")    
+## defined objects
 
-## Instantiate status
-status  = _Status("29idb:gp:text",name="status")  
+def sync_motors():
+    sync_1H = slits.center_1H.sync
+    yield from bps.abs_set(sync_1H,1)
+    sync_2H = slits.center_2H.sync
+    yield from bps.abs_set(sync_2H,1)
+    sync_1V = slits.center_1V.sync
+    yield from bps.abs_set(sync_1V,1)
+    sync_2V = slits.center_2V.sync
+    yield from bps.abs_set(sync_2V,1)
 
 
+def SetSlit1(Hsize,Vsize,Hcenter,Vcenter):
+    sync_1H = slits.center_1H.sync
+    yield from bps.abs_set(sync_1H,1)
+    sync_1V = slits.center_1V.sync
+    yield from bps.abs_set(sync_1V,1)
+    Hsize_motor = slits.size_1H.setpoint
+    Vsize_motor = slits.size_1V.setpoint
+    Hcenter_motor = slits.center_1H.setpoint
+    Vcenter_motor = slits.center_1V.setpoint
+    yield from bps.mv(Hsize_motor, Hsize, Vsize_motor, Vsize, Hcenter_motor, Hcenter, Vcenter_motor, Vcenter)
+    
 
-def SetSlit1A(Hsize,Vsize,Hcenter,Vcenter):   
-    """move slits 1A: Hsize x Vsize centered at (Hcenter,Vcenter)"""
-    caput("29idb:Slit1Hsync.PROC",1)    # make sure slits are in sink with real motors
-    caput("29idb:Slit1Vsync.PROC",1)
-    caput("29idb:Slit1Hsize.VAL", Hsize)
-    caput("29idb:Slit1Vsize.VAL", Vsize)
-    caput("29idb:Slit1Hcenter.VAL",Hcenter)
-    caput("29idb:Slit1Vcenter.VAL",Vcenter)
-    print("Slit-1A = ("+str(round(Hsize,3))+"x"+str(round(Vsize,3))+") @ ("+str(Hcenter)+","+str(Vcenter)+")")   # use status strings
-
-def SetSlit2B(Hsize,Vsize,Hcenter,Vcenter):
-    caput("29idb:Slit2Hsync.PROC",1)
-    caput("29idb:Slit2Vsync.PROC",1)
-    caput("29idb:Slit2Hsize.VAL", Hsize)
-    caput("29idb:Slit2Vsize.VAL", Vsize)
-    caput("29idb:Slit2Hcenter.VAL",Hcenter)
-    caput("29idb:Slit2Vcenter.VAL",Vcenter)
-    print("Slit-2B = ("+str(Hsize)+"x"+str(Vsize)+") @ ("+str(Hcenter)+","+str(Vcenter)+")")
+def SetSlit2(Hsize,Vsize,Hcenter,Vcenter):
+    sync_2H = slits.center_2H.sync
+    yield from bps.abs_set(sync_2H,1)
+    sync_2V = slits.center_2V.sync
+    yield from bps.abs_set(sync_2V,1)
+    Hsize_motor = slits.size_2H.setpoint
+    Vsize_motor = slits.size_2V.setpoint
+    Hcenter_motor = slits.center_2H.setpoint
+    Vcenter_motor = slits.center_2V.setpoint
+    yield from bps.mv(Hsize_motor, Hsize, Vsize_motor, Vsize, Hcenter_motor, Hcenter, Vcenter_motor, Vcenter)
+    
